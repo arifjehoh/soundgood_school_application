fdce9300601ad5d718132f6a42001ebb4d5c3b59
arifjeh <arifjeh.oh@gmail.com>
2020-12-11 14:41:55 +0100

    Actived JAR application

--- application/src/main/java/org/arifjehoh/App.java
+++ application/src/main/java/org/arifjehoh/App.java
@@ -1,43 +1,22 @@
 package org.arifjehoh;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- * Hello world!
- */
 public class App {
-    private PreparedStatement findAllInstruments;
     public static void main(String[] args) {
         System.out.println("Hello World!");
         new App().accessDB();
     }
 
     private void accessDB() {
-        try (Connection connection = createConnection()) {
-            prepareStatement(connection);
-            getInstruments();
-
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/school","arif","1234");
+            System.out.println("Connected!");
         } catch (ClassNotFoundException | SQLException exception) {
             exception.printStackTrace();
         }
     }
-
-    private void getInstruments() {
-        try (ResultSet result = findAllInstruments.executeQuery()) {
-            while (result.next()) {
-                System.out.println(result.getString(4));
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    private void prepareStatement(Connection connection) throws SQLException {
-       findAllInstruments = connection.prepareStatement("SELECT * FROM instrument_rental");
-    }
-
-    private Connection createConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/school", "arif", "1234");
-    }
 }
+
