11ab04c71e2744f6ddaa7c8e24401b5e9c7e25d1
arifjeh <arifjeh.oh@gmail.com>
2020-12-14 18:59:43 +0100

    Refactored App.java

--- application/src/main/java/org/arifjehoh/App.java
+++ application/src/main/java/org/arifjehoh/App.java
@@ -6,22 +6,5 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        new App().accessDB();
-    }
-
-    // SAMPLE OF ACCESS DATABASE
-    private void accessDB() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/school","arif","1234");
-            System.out.println("Connected!");
-        } catch (ClassNotFoundException | SQLException exception) {
-            exception.printStackTrace();
-        }
     }
 }
-
-// Completed List instruments that are available to rent. price, kind of instrument shall be shown. (Rented instrument shall not be shown)
-// TODO Rent instrument, A student can rent a instrument but only 2 instrument.
-// TODO Terminate rental, A Student can stop a ongoing rental.
-// TODO CreateConnection can maybe injected to all DAO classes?
